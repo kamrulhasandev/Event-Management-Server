c1b6ae3aeaa2e015845ab403020d3cc01c3096eb
kamrul <kamrul444216@gmail.com>
2025-06-30 01:42:17 +0600

    Fix host related issues

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,17 @@ import router from "./app/routes";
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://event-management-client-ebon.vercel.app",
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT',"PATCH", 'DELETE'],
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Worlds!");
